742c867197ccb7e0a5da79575b6da8f553093435
xavannahb <xavannahbaars@icloud.com>
2026-09-01 10:13:41 +0200

    push 4

--- public/javascript.js
+++
@@ -1,8 +0,0 @@
-const menuButton = document.querySelector("header button")
-const deNav = document.querySelector("header nav")
-
-menuButton.onclick = toggleMenu 
-
-function toggleMenu() {
-    deNav.classList.toggle("open")
-}
